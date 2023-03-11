7432577d5fa8dd57f12f766c8365319b9088e97b
Young <imlovedit@163.com>
2023-03-11 15:09:42 +0800

    update base url

--- src/config/env.ts
+++ src/config/env.ts
@@ -2,10 +2,10 @@ class GobalConfig {
     baseUrl!: string;
     constructor() {
         if (process.env.NODE_ENV == 'development') {
-            this.baseUrl = 'https://localhost:5001/api/'
+            this.baseUrl = 'https://uat.youngala.com'
         }
         else if (process.env.NODE_ENV == 'production') {
-
+            this.baseUrl = 'https://openapi.youngala.com'
         }
     }
 }
